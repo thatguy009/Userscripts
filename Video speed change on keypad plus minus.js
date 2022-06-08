285acc136527f7746c6eef39aba3f8c8b7efec28
Christopher Charles Mills <chris.mills@outlook.com.au>
2022-06-08 22:54:32 +0800

    version increment

--- Video speed change on keypad plus minus.js
+++ Video speed change on keypad plus minus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Video speed, other stuff
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Shift 2 to speed up x2, Shift 1 of regular
 // @author       You
 // @match        *://*/*
@@ -12,60 +12,56 @@
 //TODO: Automate version increments commit (pre\post?)
 
 (function () {
-    'use strict';
-    try {
-        console.log(`Keypress video changer loaded on: ${location.href}`);
-        document.addEventListener("keypress", function (evt) {
-            console.log(`Pressed key: ${evt.code}`);
-
-            if (evt.code == "NumpadAdd") {
-                RegularSpeed();
-            } else if (evt.code == "NumpadSubtract") {
-                SpeedUp();
-            }
-        });
-    } catch (error) {
-        //do nothing
+  console.log(`Keypress video changer loaded on: ${location.href}`);
+  document.addEventListener("keypress", (evt) => {
+    if (evt.code == "NumpadAdd") {
+      RegularSpeed();
+    } else if (evt.code == "NumpadSubtract") {
+      SpeedUp();
     }
+  });
 
-    function SpeedUp() {
-        for (const a of document.getElementsByTagName('video')) {
-            a.playbackRate = a.playbackRate == 1 ? 2 : a.playbackRate + 0.5;
-            console.log(`Set video ${a.src} speed to ${a.playbackRate}`);
-            ShowSpeedChange(a);
-        }
+  function SpeedUp() {
+    for (const a of document.getElementsByTagName("video")) {
+      a.playbackRate = a.playbackRate == 1 ? 2 : a.playbackRate + 0.5;
+      ShowSpeedChange(a);
     }
+  }
 
-    function RegularSpeed() {
-        for (const b of document.getElementsByTagName('video')) {
-            b.playbackRate = 1;
-            ShowSpeedChange(b);
-        }
-        console.log("Set video speed to 1");
+  function RegularSpeed() {
+    for (const b of document.getElementsByTagName("video")) {
+      b.playbackRate = 1;
+      ShowSpeedChange(b);
     }
+  }
 
-    //TODO: Test this on a variety of websites
-    function ShowSpeedChange(vid) {
-        const rect = vid.getBoundingClientRect();
-        const txtID = `_speedText_${Date.parse(new Date())}${Math.random()}`;
-        const innerText = `Speed set to: x${vid.playbackRate}`;
+  function ShowSpeedChange(vid) {
+    const rect = vid.getBoundingClientRect();
+    const txtID = `_speedText_${Date.parse(new Date())}${Math.random()}`;
+    const innerText = `Speed set to: x${vid.playbackRate}`;
 
-        const speedText = document.createElement("p");
+    const speedText = document.createElement("p");
 
-        speedText.className = txtID;
-        speedText.style.fontSize = "large";
-        speedText.style.color = "white";
-        speedText.style.position = "fixed";
-        speedText.style.top = `${(rect.top + 20).toString()}px`;
-        speedText.style.left = `${(rect.left + 20).toString()}px`;
-        speedText.style.zIndex = vid.style.zIndex + 100;
-        speedText.style.textShadow = "black 0px 0px 8px";
-        speedText.innerText = innerText;
+    speedText.className = txtID;
+    speedText.style.fontSize = "large";
+    speedText.style.color = "white";
+    speedText.style.position = "fixed";
+    speedText.style.top = `${(rect.top + 20).toString()}px`;
+    speedText.style.left = `${(rect.left + 20).toString()}px`;
+    speedText.style.zIndex = vid.style.zIndex + 100;
+    speedText.style.textShadow = "black 0px 0px 8px";
+    speedText.innerText = innerText;
 
-        document.body.appendChild(speedText);
+    document.body.appendChild(speedText);
 
-        console.log(`${txtID} added to page with text: ${innerText}`);
+    console.log(`Set video ${vid.src} speed to ${vid.playbackRate}`);
 
-        setTimeout(() => document.querySelectorAll("[class^='_speedText_']").forEach(e => e.remove()), 250);
-    }
+    setTimeout(
+      () =>
+        document
+          .querySelectorAll("[class^='_speedText_']")
+          .forEach((e) => e.remove()),
+      250,
+    );
+  }
 })();
