29d096a6fcf412ca6736eeaafcce1679baf6e6e9
Christopher Mills <chris.mills@outlook.com.au>
2022-07-17 22:25:39 +0800

    Added Custom context menu.js

---
+++ Custom context menu.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Custom context menu
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  custom context menu for various sites
+// @author       You
+// @run-at       context-menu
+// @match        *://*/*
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+function test() {
+    alert('Hi');
+}
+
+
+if (location.hostname == "e621.net") {
+    const s = location.search;
+    const search = unescape(s.substring(s.indexOf('=') + 1).split('&')[0]).split('+').filter(t => t.indexOf('order:') == -1);
+
+    ["-id", "id", "score"].forEach(searchType => GM_registerMenuCommand(`Sort by $(searchType)`, () => {
+        location.search = `?tags=${escape(search.join('+'))}+order:${escape(searchType)}`;
+    }));
+
+    ["score:>=100", "favcount:>=100"].forEach(filterType => GM_registerMenuCommand(`Add Filter $(filterType)`, () => {
+        search.push(filterType);
+        location.search = `?tags=${escape(search.join('+'))}`;
+    }));
+}
