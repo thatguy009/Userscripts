ff4ffd111896e6309dad5ab24662cc1883daee6d
Christopher Mills <chris.mills@outlook.com.au>
2022-07-18 22:38:30 +0800

    context test

--- Custom context menu.js
+++ Custom context menu.js
@@ -9,15 +9,24 @@
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 
+GM_registerMenuCommand("Default menu", () => {
+    alert(`Called Default menu on ${location.href}`);
+    debugger;
+});
+
+["Thing 1", "Thing 2", "Thing 3"].forEach(thing => GM_registerMenuCommand(`Alert for ${thing}`, () => {
+    alert(thing);
+}));
+
 if (location.hostname == "e621.net") {
     const s = location.search;
     const search = unescape(s.substring(s.indexOf('=') + 1).split('&')[0]).split('+').filter(t => t.indexOf('order:') == -1);
 
-    ["-id", "id", "score"].forEach(searchType => GM_registerMenuCommand(`Sort by $(searchType)`, () => {
+    ["-id", "id", "score"].forEach(searchType => GM_registerMenuCommand(`Sort by ${searchType}`, () => {
         location.search = `?tags=${escape(search.join('+'))}+order:${escape(searchType)}`;
     }));
 
-    ["score:>=100", "favcount:>=100"].forEach(filterType => GM_registerMenuCommand(`Add Filter $(filterType)`, () => {
+    ["score:>=100", "favcount:>=100"].forEach(filterType => GM_registerMenuCommand(`Add Filter ${filterType}`, () => {
         search.push(filterType);
         location.search = `?tags=${escape(search.join('+'))}`;
     }));
